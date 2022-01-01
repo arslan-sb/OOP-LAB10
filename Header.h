29cf4abf389202279820c2f2b5518cc65fc339d4
Arslan <01-134211-053@student.bahria.edu.pk>
2022-01-01 16:00:54 +0500

    Added B constructor

--- Header.h
+++ Header.h
@@ -5,8 +5,15 @@ private:
 	int y;
 public:
 	A() :x(0), y(0) {}
+
 	void diplay() {
 		cout << "x: " << x << endl;
 		cout << "y: " << y << endl;
 	}
+};
+class B :public A {
+private: 
+	int z;
+public:
+	B() :A(), z(0) {}
 };
